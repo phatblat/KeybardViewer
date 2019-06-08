f9588cfe8718bc3929a6fe8156cba9e6329ee4c2
Ben Chatelain <ben@octop.ad>
2019-06-07 20:46:09 -0600

    Initial Commit

---
+++ KeybardViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KeybardViewer
+//
+//  Created by Ben Chatelain on 6/7/19.
+//  Copyright © 2019 Ben Chatelain. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
